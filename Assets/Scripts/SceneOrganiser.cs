40af142c139faf6f7b76d555ba83278283dacb0d
unknown <simenf94@hotmail.com>
2022-04-05 19:05:32 +0200

    calculateboundingbox

--- Assets/Scripts/SceneOrganiser.cs
+++ Assets/Scripts/SceneOrganiser.cs
@@ -9,7 +9,7 @@ public class SceneOrganiser : MonoBehaviour
     /// Allows this class to behave like a singleton
     /// </summary>
     public static SceneOrganiser Instance;
-
+    Camera cam;
     /// <summary>
     /// The cursor object attached to the Main Camera
     /// </summary>
@@ -133,7 +133,7 @@ public class SceneOrganiser : MonoBehaviour
         quadRenderer = quad.GetComponent<Renderer>();
 
         // Makes quad invincible
-        //quad.GetComponent<Renderer>().enabled = false;
+        quad.GetComponent<Renderer>().enabled = false;
         // Here you can set the transparency of the quad. Useful for debugging
         float transparency = 0f;
         quadRenderer.material.color = new Color(1, 1, 1, transparency);
@@ -157,7 +157,6 @@ public class SceneOrganiser : MonoBehaviour
     /// </summary>
     public void FinaliseLabel(AnalysisRootObject analysisObject)
     {
-        
         if (analysisObject.predictions != null)
         {
             lastLabelPlacedText = lastLabelPlaced.GetComponent<TextMesh>();
@@ -169,7 +168,7 @@ public class SceneOrganiser : MonoBehaviour
 
             if (bestPrediction.probability > probabilityThreshold)
             {
-                quadRenderer = quad.GetComponent<Renderer>();
+                quadRenderer = quad.GetComponent<Renderer>() as Renderer;
                 Bounds quadBounds = quadRenderer.bounds;
 
                 // Position the label as close as possible to the Bounding Box of the prediction 
@@ -179,32 +178,32 @@ public class SceneOrganiser : MonoBehaviour
 
                 // Set the tag text
                 lastLabelPlacedText.text = bestPrediction.tagName;
-                //Does raycast straight forward positions label where the raycast hits.
+
+                // Cast a ray from the user's head to the currently placed label, it should hit the object detected by the Service.
+                // At that point it will reposition the label where the ray HL sensor collides with the object,
+                // (using the HL spatial tracking)
                 Debug.Log("Repositioning Label");
-                RaycastHit hitInfo;
                 Vector3 headPosition = Camera.main.transform.position;
-                Vector3 objDirection = Camera.main.transform.forward;
+                RaycastHit objHitInfo;
+                Vector3 objDirection= lastLabelPlaced.position;
+                cam = GetComponent<Camera>();
 
-                if (Physics.Raycast(
-                        headPosition,
-                        objDirection,
-                        out hitInfo,
-                        20.0f,
-                        Physics.DefaultRaycastLayers))
+                Ray ray = cam.ViewportPointToRay(objDirection);
+
+                Debug.Log(objDirection);
+                Debug.Log(ray.direction);
+                Debug.Log(Camera.main.transform.forward);
+                if (Physics.Raycast(Camera.main.transform.position, objDirection, out objHitInfo, 20.0f, Physics.DefaultRaycastLayers))
                 {
-                    // If the Raycast has succeeded and hit a hologram
-                    // hitInfo's point represents the position being gazed at
-                    // hitInfo's collider GameObject represents the hologram being gazed at
-
-                    lastLabelPlaced.position = hitInfo.point;
-
+                    lastLabelPlaced.transform.position = objHitInfo.point;
+                    Debug.Log("Position " + lastLabelPlaced.transform.position);
                 }
 
             }
         }
         // Reset the color of the cursor
         cursor.GetComponent<Renderer>().material.color = Color.green;
-
+        quad.GetComponent<Renderer>().enabled = true;
         // Stop the analysis process
         ImageCapture.Instance.ResetImageCapture();
     }
@@ -218,18 +217,48 @@ public class SceneOrganiser : MonoBehaviour
         Debug.Log($"BB: left {boundingBox.left}, top {boundingBox.top}, width {boundingBox.width}, height {boundingBox.height}");
 
         double centerFromLeft = boundingBox.left + (boundingBox.width / 2);
-        double centerFromTop = (boundingBox.height / 2);
+        double centerFromTop = boundingBox.top + (boundingBox.height / 2);
         Debug.Log($"BB CenterFromLeft {centerFromLeft}, CenterFromTop {centerFromTop}");
 
         double quadWidth = b.size.normalized.x;
         double quadHeight = b.size.normalized.y;
         Debug.Log($"Quad Width {b.size.normalized.x}, Quad Height {b.size.normalized.y}");
-
-        double normalisedPos_X = (quadWidth * centerFromLeft) - (quadWidth / 2);
-        double normalisedPos_Y = (quadHeight * centerFromTop) - (quadHeight / 2);
-
-        return new Vector3((float)normalisedPos_X, (float)normalisedPos_Y, 0);
-
+        if ((boundingBox.top > 0.5) & (boundingBox.left > 0.5))
+        {
+            double normalisedPos_X = (centerFromLeft - 0.5);
+            double normalisedPos_Y = (0.5 - centerFromTop);
+            Debug.Log($"1 X {normalisedPos_X}, Y {normalisedPos_Y}");
+            return new Vector3((float)normalisedPos_X, (float)normalisedPos_Y, 0);
+        }
+        else if ((boundingBox.top > 0.5) & (boundingBox.left < 0.5))
+        {
+            double normalisedPos_X = (centerFromLeft - 0.5);
+            double normalisedPos_Y = (0.5 - centerFromTop);
+            Debug.Log($"2 X {normalisedPos_X}, Y {normalisedPos_Y}");
+            return new Vector3((float)normalisedPos_X, (float)normalisedPos_Y, 0);
+        }
+        else if((boundingBox.top < 0.5) & (boundingBox.left < 0.5))
+        {
+            double normalisedPos_X = (centerFromLeft - 0.5);
+            double normalisedPos_Y = (0.5 - centerFromTop);
+            Debug.Log($"3 X {normalisedPos_X}, Y {normalisedPos_Y}");
+            return new Vector3((float)normalisedPos_X, (float)normalisedPos_Y, 0);
+        }
+        else if((boundingBox.top < 0.5) & (boundingBox.left > 0.5))
+        {
+            double normalisedPos_X = (centerFromLeft - 0.5) + (quadWidth / 2);
+            double normalisedPos_Y = (0.5 - centerFromTop) + (quadHeight / 2);
+            Debug.Log($"4 X {normalisedPos_X}, Y {normalisedPos_Y}");
+            return new Vector3((float)normalisedPos_X, (float)normalisedPos_Y, 0);
+        }
+        else
+        {
+            return new Vector3(0, 0, 0);
+        }
+        //double normalisedPos_X = (quadWidth * centerFromLeft) - (quadWidth / 2);
+        //double normalisedPos_Y = (quadHeight * centerFromTop) - (quadHeight / 2);
+        //Debug.Log($"X {normalisedPos_X}, Y {normalisedPos_Y}");
+        //return new Vector3((float)normalisedPos_X, (float)normalisedPos_Y, 0);
     }
     private void reload()
     {
